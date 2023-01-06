edf8973d6e58f49f6a359cc5b8d2de068eb37f8b
manish <manishkumar60708090@gmail.com>
2023-01-06 13:24:56 +0530

    Environment variables

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const { readdirSync } = require("fs");
 const app = express();
@@ -7,6 +9,8 @@ app.use(cors());
 
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.listen(8000, () => {
-  console.log("server is lestining...");
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("server is lestining... hello how are you?");
 });
