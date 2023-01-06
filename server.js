7d1f932a0690917d15e4a566aa00e31a15e671bb
manish <manishkumar60708090@gmail.com>
2023-01-06 13:50:00 +0530

    Let's setup a mongodb database

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const mongoose = require("mongoose");
+
 const { readdirSync } = require("fs");
 const app = express();
 app.use(cors());
 
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+// database
+mongoose
+  .connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB CONNECTED are successfully established"))
+  .catch((err) => console.log("DB CONNECTION ERR", err));
 
 const PORT = process.env.PORT || 8000;
 
